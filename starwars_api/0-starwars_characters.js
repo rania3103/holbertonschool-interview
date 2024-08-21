1b43158f474bc0bbb7bf0d84560c1f7c00393f22
rania3103 <raniaabassi33@gmail.com>
2024-08-21 10:19:06 +0100

    fix semistandard

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-const request = require("request");
-const movie_id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, async (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const chars = JSON.parse(body).characters;
-    for (const char_url of chars) {
+    for (const charUrl of chars) {
       await new Promise((resolve, reject) => {
-        request(char_url, async (error, response, body) => {
+        request(charUrl, async (error, response, body) => {
           if (!error && response.statusCode === 200) {
             console.log(JSON.parse(body).name);
           }
