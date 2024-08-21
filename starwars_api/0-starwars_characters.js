5b1255893f533046185eb5a2b77a55be1c3d0bc1
rania3103 <raniaabassi33@gmail.com>
2024-08-21 10:17:08 +0100

    fix it

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ const url = `https://swapi-api.hbtn.io/api/films/${movie_id}`;
 
 request(url, async (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    const chars = await JSON.parse(body).characters;
+    const chars = JSON.parse(body).characters;
     for (const char_url of chars) {
       await new Promise((resolve, reject) => {
         request(char_url, async (error, response, body) => {
