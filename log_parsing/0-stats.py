890bf66bfbb4c9fa42a3d8172398b5d1679c1b1e
rania3103 <raniaabassi33@gmail.com>
2024-09-16 12:03:56 +0100

    try to fix it

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -4,32 +4,33 @@ import sys
 
 if __name__ == "__main__":
     status_code = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0}
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0}
     numb_lines = 0
     tot_size = 0
 
     def print_statistics():
         """a function that prints statistics of the log"""
         print(f'File size: {tot_size}')
-        for code in sorted(status_code.keys()):
-            if status_code[code] > 0:
-                print(f'{code}: {status_code[code]}')
+        for code, count in sorted(status_code.items()):
+            if count > 0:
+                print(f'{code}: {count}')
 
     try:
         for line in sys.stdin:
             try:
                 words = line.split(' ')
-                if len(words) == 6:
-                    tot_size += int(words[5])
-                    code = int(words[4])
-                    if code in status_code.keys():
+                if len(words) > 2:
+                    file_size = int(words[-1])
+                    code = words[-2]
+                    tot_size += file_size
+                    if code in status_code:
                         status_code[code] += 1
                     numb_lines += 1
                     if numb_lines % 10 == 0:
