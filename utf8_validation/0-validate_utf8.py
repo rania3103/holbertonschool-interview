e32b2809550cbe94b97db0e15390655b89e7741a
rania3103 <raniaabassi33@gmail.com>
2024-09-16 13:19:08 +0100

    fix pep8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a method that determines if a given data set represents a valid UTF-8 encoding."""
+"""a method that determines if a given data set represents
+a valid UTF-8 encoding."""
 
 
 def validUTF8(data):
