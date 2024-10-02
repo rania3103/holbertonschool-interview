be8b988dac8db03acee9d055152acc220eea9048
rania3103 <raniaabassi33@gmail.com>
2024-10-02 13:08:48 +0100

    add files

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" roatate an n x n 2D matrix 90 degrees clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    """The matrix will be edited in-place."""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i].reverse()
