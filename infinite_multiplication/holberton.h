6201a06cdf9fc565c0d198c0f15f357d9dd70d36
rania3103 <raniaabassi33@gmail.com>
2024-10-02 10:34:27 +0100

    add header file

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,8 @@
+#ifndef INFMUL
+#define INFMUL
+#include <stdlib.h>
+int _putchar(char c);
+int is_digit(char *str);
+void print_complete_string(char *ch);
+char *multiply(char *num1, char*num2);
+#endif
