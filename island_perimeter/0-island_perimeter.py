c73e2cb82dfd1631ab795c625b0c2657918943fe
rania3103 <raniaabassi33@gmail.com>
2024-11-11 08:05:02 +0100

    fix function doc

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -3,9 +3,8 @@
 
 
 def island_perimeter(grid):
-    """The grid is completely surrounded by water,
-    There is only one island (or nothing),
-    0 represents water and 1 represents land"""
+    """The grid is a list of list of integers:0 represents water
+    and 1 represents land"""
     n_rows = len(grid)
     n_cols = len(grid[0])
     p = 0
