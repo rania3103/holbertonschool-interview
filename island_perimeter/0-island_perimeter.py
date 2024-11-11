8c364992222635e72a1ae525a81ff5ed41198e5e
rania3103 <raniaabassi33@gmail.com>
2024-11-11 07:50:45 +0100

    Island Perimeter

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a function that returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """0 represents water
+    1 represents land"""
+    n_rows = len(grid)
+    n_cols = len(grid[0])
+    p = 0
+    for row in range(n_rows):
+        for col in range(n_cols):
+            if grid[row][col] == 1:
+                # top
+                if row == 0 or grid[row - 1][col] == 0:
+                    p += 1
+                # bottom
+                if row == n_rows - 1 or grid[row + 1][col] == 0:
+                    p += 1
+                # left
+                if col == 0 or grid[row][col - 1] == 0:
+                    p += 1
+                # right
+                if col == n_cols - 1 or grid[row][col + 1] == 0:
+                    p += 1
+    return p
