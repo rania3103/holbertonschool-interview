29bef23b0a4a97f440f940783f199f641b0a52cf
rania3103 <raniaabassi33@gmail.com>
2024-11-15 07:31:37 +0100

    back to prev impl and try to fix documenetation

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""a function that returns the perimeter of the island described in grid"""
+"""
+a function that returns the perimeter of the island described in grid
+"""
 
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid."""
+    """
+    returns the perimeter of the island described in grid.
+    (0 represents water,1 represents land)
+    """
     if not grid or not grid[0]:
         return 0
     n_rows = len(grid)
@@ -12,9 +17,12 @@ def island_perimeter(grid):
     for row in range(n_rows):
         for col in range(n_cols):
             if grid[row][col] == 1:
-                p += 4
-                if row > 0 and grid[row - 1][col] == 1:
-                    p -= 2
-                if col > 0 and grid[row][col - 1] == 1:
-                    p -= 2
+                if row == 0 or grid[row - 1][col] == 0:
+                    p += 1
+                if row == n_rows - 1 or grid[row + 1][col] == 0:
+                    p += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    p += 1
+                if col == n_cols - 1 or grid[row][col + 1] == 0:
+                    p += 1
     return p
