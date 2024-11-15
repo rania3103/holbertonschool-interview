267315346aba01af169d992123d24e475e881427
rania3103 <raniaabassi33@gmail.com>
2024-11-15 07:28:48 +0100

    try a different approach to fix the error

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -13,12 +13,9 @@ def island_perimeter(grid):
     for row in range(n_rows):
         for col in range(n_cols):
             if grid[row][col] == 1:
-                if row == 0 or grid[row - 1][col] == 0:
-                    p += 1
-                if row == n_rows - 1 or grid[row + 1][col] == 0:
-                    p += 1
-                if col == 0 or grid[row][col - 1] == 0:
-                    p += 1
-                if col == n_cols - 1 or grid[row][col + 1] == 0:
-                    p += 1
+                p += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    p -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    p -= 2
     return p
